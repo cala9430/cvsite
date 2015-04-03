3d14a370ad41c6485e96a03f963b50335da90c78
Daniel <calabuig.daniel@gmail.com>
2015-04-03 19:50:16 -0300

    Public Folder
    Make public folder accesible

--- app.js
+++ app.js
@@ -5,15 +5,19 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+//Make public folder accessible as static content
+app.use('/public', express.static(__dirname + '/public'));
+
+//Configure Jade
 app.set('views', './pages');
 app.set('view engine', 'jade');
 
-
+//Basic routing
 app.get('/', function (req, res) {
 	res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
+//Start server
 var server = app.listen(3000, function configureServer() {
 
 	var host = server.address().address;
