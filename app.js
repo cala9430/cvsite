4bb56f1c81bca92bdea9df2b0f11c12c2889ae42
Daniel <calabuig.daniel@gmail.com>
2015-04-03 19:06:54 -0300

    Jade and Index
    Add Jade to npm
    Serve index.jade

--- app.js
+++ app.js
@@ -5,11 +5,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.set('views', './pages');
+app.set('view engine', 'jade');
+
+
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function configureServer() {
 
 	var host = server.address().address;
 	var port = server.address().port;
@@ -17,3 +22,4 @@ var server = app.listen(3000, function () {
 	console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
